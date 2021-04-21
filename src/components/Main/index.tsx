22ee58b9f160ca5fe5a5145b2b18e8da13f0b8aa
dmarins <diogodsfmarins@gmail.com>
2021-04-21 15:58:01 -0300

    criando a main + testes de unidade + styled components

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import * as S from "./styles";
+
 const Main = () => (
-  <main>
-    <h2>React Avançado</h2>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="imagem de um átomo e React Avançado escrito ao lado."
+    />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJS e Styled Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="ilustração de um programador sentado de frente para uma tela com códigos."
+    />
+  </S.Wrapper>
 );
 
 export default Main;
